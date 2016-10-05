dd75051f3b5de3befc736361b4def2ba98069415
chriskretler <ckretler@umich.edu>
2016-10-05 06:21:26 -0400

    removed any traces of friendliness.

--- front_page.rb
+++ front_page.rb
@@ -234,7 +234,7 @@ pre {
 
 
 			<h2>Health</h2>
-				<p>Hello, application health can be monitored at <a href="./health">this url.</a></p>
+				<p>Application health can be monitored at <a href="./health">this url.</a></p>
 				
 				<p>This functionality can be stymied by clicking <a href="./health_hider">here.</a></p>
 
