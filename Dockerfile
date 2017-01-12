a01dd4c36948845495ebe921b9134506e7c14e65
chriskretler <ckretler@umich.edu>
2017-01-12 11:01:28 -0500

    combine thin, example and html sample applications.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,26 @@
-#FROM alpine:3.4
 FROM ubuntu:latest
 
 MAINTAINER Chris Kretler <ckretler@umich.edu>
 
-# Install ruby
-#RUN apk add --no-cache ruby ruby-rack
+# Install ruby and necessary libraries for building thin.
 RUN apt-get update \ 
-	&& apt-get install -y ruby ruby-rack
+	&& apt-get install -y ruby ruby-rack ruby-dev build-essential
 
-RUN gem install -N rack-rewrite rack-cache
+# Install thin.
+RUN gem install -N thin
 
-# create place for app to run from
+# Expose the port on which the app will run.
+EXPOSE 3001
+
+# Set a sample environmental variable
+ENV ENVVAR howdy
+
+# Clean up build reqs that aren't necessary for deployment.
+RUN apt-get remove -y build-essential ruby-dev \
+	&& apt-get autoremove -y
+	
+# Create place for app to run from
 WORKDIR /app/
 COPY . /app/
 
-# expose the port on which the app will run.
-EXPOSE 8080
-
-CMD rackup --host 0.0.0.0 -p 8080
-#CMD /bin/sh
+CMD thin start -R config.ru -p 3001
--- front_page.rb
+++ front_page.rb
@@ -6,7 +6,7 @@ class FrontPage
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Basic Ruby Application</title>
+  <title>Basic Docker Application for Ruby</title>
 
 
 <style>
@@ -208,74 +208,68 @@ pre {
 </head>
 <body>
 
-<section class='container'>
+<section class='container' style="background-color: #FFC72C; color: #041E42">
           <hgroup>
-            <h1>Welcome to Chris's Demo Application</h1>
+            <h1>Welcome to a Sample Application</h1>
           </hgroup>
 
 
         <div class="row">
-          <section class='col-xs-12 col-sm-6 col-md-6'>
-            <section>
-              <h2>Basic Functions</h2>
-                <p>
-                  There are some basic questions available at the <a href="./basic_questions">basic questions page</a>.
-                </p>
-
+          <section class='col-xs-12 col-sm-6 col-md-6' style="background-color: #FFC72C; color: #041E42">
+            <section >
+            
+              <h2>Basic Questions</h2>
+                  Challenge your knowledge of the University of Michigan's history <a href="./basic_questions">here</a>.
+            
+              <h2>Useful Links & Documentation</h2>
 <ol>
-  <li>From the Web Console homepage, navigate to your project</li>
-  <li>Click on Browse &gt; Builds</li>
-  <li>From the view for your Build click on the button to copy your GitHub webhook</li>
-  <li>Navigate to your repository on GitHub and click on repository settings &gt; webhooks</li>
-  <li>Paste your webhook URL provided by OpenShift &mdash; that's it!</li>
+  <li><a href="https://hub.docker.com">Docker Hub</a> contains an amazing number of pre-built images</li>
+  <li><a href="https://github.com/docker/labs">This</a> github repo contains tutorials for several languages.</li>
+  <li><a href="https://kubernetes.io">Kubernetes</a> is a system which provides health checks, auto-scaling and internal networking for docker containers.</li>  
+  <li><a href="https://docs.docker.com/">The documentation center</a> at Docker's website provides links to docker commands and installation guides.</li>
+  <li>Docker containers can be run in the cloud in several locations.  One of them is  Amazon's <a href="https://aws.amazon.com/ecs/">ECS</a>.</li>
 </ol>
-<p>After you save your webhook, if you refresh your settings page you can see the status of the ping that Github sent to OpenShift to verify it can reach the server.</p>
-<p>Note: adding a webhook requires your OpenShift server to be reachable from GitHub.</p>
 
-
-			<h2>Health</h2>
-				<p>Application health can be monitored at <a href="./health">this url.  Don't tell me that isn't interesting.</a></p>
-				
-				<p>This functionality can be stymied by clicking <a href="./health_hider">here.</a></p>
-
-				<p>When running in OpenShift, the pod name will display here: #{ENV["HOSTNAME"]}</p>
-				
-				<p>The value of the environmental variable called howdy is: #{ENV["HOWDY_VALUE"]}</p>
+			<h2>Application Health & Other Information</h2>
+<ol>
+	<li>Application health can be monitored at <a href="./health">this url. </a></li>
+	<li>The server/pod name is: #{ENV["HOSTNAME"]}</li>		
+	<li>The value of the environmental variable is: #{ENV["ENVVAR"]}</li>
+</ol>
  
-      </section>
-          </section>
-          <section class="col-xs-12 col-sm-6 col-md-6">
+			</section>
+    	</section>
+          <section class="col-xs-12 col-sm-6 col-md-6" style="background-color: #FFC72C; color: #041E42">
 
-                <h2>Basic workflow</h2>
-                
-                <h3>Local Changes</h3>
-                <p>The basic idea is that a developer would still follow her same basic procedure of making changes locally.  As this is a rack application I used <a href="http://rubylearning.com/blog/2013/04/02/whats-rack/">rackup</a> to test my application locally. </p>
+                <h2>Useful Commands</h2>
 
-                <h3>Command Line</h3>
-                <p>With the <a href="http://docs.openshift.org/latest/cli_reference/overview.html">OpenShift command line interface</a> (CLI), you can create applications and manage projects from a terminal.</p>
+<pre># Build from a dockerfile in the local directory.
+$ docker build -t {your-docker-tag-name} .
 
-<pre># Create a project and application
-$ oc new-project cop-cloud
-$ oc new-app openshift/ruby-20-centos7~https://github.com/chriskretler/ruby-ex
-# Commit your changes and push to OpenShift
-$ git commit -a -m 'Some commit message'
-$ git push
-$ oc logs &lt;your pod name goes here&gt;
-# if your pod is crashing frequently, append a -p after the pod name to get the logs.</pre>
-                
-                <h3>Web Console</h3>
-                <p>You can use the Web Console to view the state of your application components and launch new builds.</p>
+# Build from a dockerfile in a github repository.
+$ docker build -t {your-docker-tag-name} https://github.com/chriskretler/nginx-demo
+
+# Run the nginx container from docker hub with port 80 exposed on your local computer.
+$ docker run -p 80:80 nginx
+
+# Run your docker container
+$ docker run -p 80:80 {your-docker-tag-name}
+
+# List all of your running containers
+$ docker ps
+
+# List all images in your current registry
+$ docker images
+</pre>
 
 
           </section>
         </div>
-
-        <footer>
-          <div class="logo"><a href="https://www.openshift.com/"></a></div>
-        </footer>
 </section>
 
+	<section class='col-xs-12 col-sm-6 col-md-6' >
 
+	
 </body>
 </html>
 MAIN_PAGE_CONTENTS
